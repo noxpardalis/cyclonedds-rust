e71cb405daa9214d9a07950497248fcd372d3e05
Troy Karan Harrison <troy.harrison@zettascale.tech>
2025-08-29 17:49:08 +0200

    Add default nix devshell
    Commit-type: build

---
+++ nix/devshell.nix
@@ -0,0 +1,29 @@
+{
+  pkgs,
+  inputs,
+  perSystem,
+  ...
+}:
+
+let
+  src = ../.;
+  rust-toolchain = pkgs.rust-bin.fromRustupToolchainFile "${src}/rust-toolchain.toml";
+  crane-lib = (inputs.crane.mkLib pkgs).overrideToolchain rust-toolchain;
+in
+crane-lib.devShell {
+  LIBCLANG_PATH = "${pkgs.libclang.lib}/lib";
+  inputsFrom = [ perSystem.self.default ];
+
+  packages =
+    with pkgs;
+    [
+      # TODO also add this via `crane`.
+      cargo-audit
+      cargo-deny
+      cargo-nextest
+
+      perSystem.self.cyclonedds-c
+    ]
+    ++ lib.optionals stdenv.isLinux [ cargo-llvm-cov ];
+
+}
