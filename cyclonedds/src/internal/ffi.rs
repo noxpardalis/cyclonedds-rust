0caa260383c02b1652f3d6ac4965a743c036996f
Troy Karan Harrison <troy.harrison@zettascale.tech>
2025-08-31 23:14:40 +0200

    Implement `sertype` and `serdata` extension points
    This also adds tests for exercising the `ref` and `unref` functions for
    the `sertype` and `serdata` types because they might not be exercised in
    normal usage but might be useful for consumers of the internal API,
    
    Commit-type: feature
    Scope: cyclonedds

--- cyclonedds/src/internal/ffi.rs
+++ cyclonedds/src/internal/ffi.rs
@@ -5,6 +5,9 @@
 
 #![allow(unsafe_code)]
 
+pub mod serdata_ops;
+pub mod sertype_ops;
+
 use crate::Result;
 use crate::error::IntoError;
 
@@ -50,3 +53,78 @@ pub fn dds_set_status_mask(entity: cyclonedds_sys::dds_entity_t, mask: u32) -> R
     unsafe { cyclonedds_sys::dds_set_status_mask(entity, mask) }.into_error()?;
     Ok(())
 }
+
+/// Create a new serdata. This is primarily used by the
+/// [`Serdata`][`crate::internal::serdata::Serdata`] wrapper.
+pub fn ddsi_serdata_new(
+    sertype: &cyclonedds_sys::ddsi_sertype,
+    kind: cyclonedds_sys::ddsi_serdata_kind,
+) -> cyclonedds_sys::ddsi_serdata {
+    let mut serdata = cyclonedds_sys::ddsi_serdata::default();
+
+    unsafe { cyclonedds_sys::ddsi_serdata_init(&mut serdata, sertype, kind) }
+    serdata
+}
+
+/// Increment the reference count of a serdata. This is primarily used by the
+/// [`Serdata`][`crate::internal::serdata::Serdata`] wrapper.
+#[inline]
+pub fn ddsi_serdata_ref(serdata: &mut cyclonedds_sys::ddsi_serdata) {
+    unsafe { cyclonedds_sys::ddsi_serdata_ref(serdata) };
+}
+
+/// Decrement the reference count of a serdata. This is primarily used by the
+/// [`Serdata`][`crate::internal::serdata::Serdata`] wrapper.
+#[inline]
+pub fn ddsi_serdata_unref(serdata: &mut cyclonedds_sys::ddsi_serdata) {
+    unsafe { cyclonedds_sys::ddsi_serdata_unref(serdata) };
+}
+
+/// Create a new sertype. This is primarily used by the
+/// [`Sertype`][`crate::internal::sertype::Sertype`] wrapper.
+pub fn ddsi_sertype_new(
+    type_name: &std::ffi::CStr,
+    sertype_ops: &cyclonedds_sys::ddsi_sertype_ops,
+    serdata_ops: &cyclonedds_sys::ddsi_serdata_ops,
+    topic_has_key: bool,
+) -> cyclonedds_sys::ddsi_sertype {
+    let mut sertype = cyclonedds_sys::ddsi_sertype::default();
+
+    unsafe {
+        cyclonedds_sys::ddsi_sertype_init(
+            &mut sertype,
+            type_name.as_ptr(),
+            sertype_ops,
+            serdata_ops,
+            // the interface is based around topic_kind_no_key so invert here.
+            !topic_has_key,
+        );
+    };
+
+    sertype
+}
+
+/// Increment the reference count of a sertype. This is primarily used by the
+/// [`Sertype`][`crate::internal::sertype::Sertype`] wrapper.
+#[inline]
+pub fn ddsi_sertype_ref(sertype: &mut cyclonedds_sys::ddsi_sertype) {
+    unsafe { cyclonedds_sys::ddsi_sertype_ref(sertype) };
+}
+
+/// Decrement the reference count of a sertype. This is primarily used by the
+/// [`Sertype`][`crate::internal::sertype::Sertype`] wrapper.
+#[inline]
+pub fn ddsi_sertype_unref(sertype: &mut cyclonedds_sys::ddsi_sertype) {
+    unsafe { cyclonedds_sys::ddsi_sertype_unref(sertype) };
+}
+
+/// Finalize a sertype. This is primarily used by the
+/// [`Sertype`][`crate::internal::sertype::Sertype`] wrapper.
+pub fn ddsi_sertype_fini(sertype: &mut cyclonedds_sys::ddsi_sertype) {
+    unsafe {
+        cyclonedds_sys::ddsi_sertype_fini(sertype);
+    }
+}
+
+#[cfg(test)]
+mod tests;
