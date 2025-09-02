0367387ba9e0a08c0304df9eea71a35b20b8bda2
Troy Karan Harrison <troy.harrison@zettascale.tech>
2025-09-03 01:54:07 +0200

    Add participant
    Commit-type: feature
    Subject: cyclonedds

--- cyclonedds/src/lib.rs
+++ cyclonedds/src/lib.rs
@@ -1,6 +1,7 @@
 mod domain;
 mod duration;
 mod error;
+mod participant;
 pub mod qos;
 pub mod state;
 pub mod status;
@@ -8,6 +9,7 @@ pub mod status;
 pub use domain::Domain;
 pub use duration::Duration;
 pub use error::{Error, Result};
+pub use participant::Participant;
 pub use qos::QoS;
 pub use state::State;
 pub use status::Status;
