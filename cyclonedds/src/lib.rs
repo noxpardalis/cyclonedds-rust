85662c7178206a6310ae8d41a9d36b6972852aba
Troy Karan Harrison <troy.harrison@zettascale.tech>
2025-09-09 22:08:54 +0200

    Add time type
    Commit-type: feature
    Scope: cyclonedds

--- cyclonedds/src/lib.rs
+++ cyclonedds/src/lib.rs
@@ -4,12 +4,14 @@ pub mod entity;
 mod error;
 pub mod state;
 pub mod status;
+mod time;
 
 pub use domain::Domain;
 pub use duration::Duration;
 pub use error::{Error, Result};
 pub use state::State;
 pub use status::Status;
+pub use time::Time;
 
 #[cfg(feature = "internal")]
 pub mod internal;
