5b534b4ca0f015fc066f5b550f7556dfb4a079a8
Troy Karan Harrison <troy.harrison@zettascale.tech>
2025-09-12 02:03:36 +0200

    Add Entity trait
    The `Entity` trait exposes operations common to all DDS entities:
    
    - getting the entity ID
    - getting the instance handle
    - reading and taking statuses
    - getting the status changes
    
    A placeholder test is included to validate the `Entity` trait implementation for
    each entity type; it will be extended as the different entity types are
    implemented.
    
    This also introduces the `ffi` module, providing ergonomic wrappers around
    the `cyclonedds-sys` crate. All unsafe code is confined to this module.
    
    Commit-type: feature
    Scope: cyclonedds

---
+++ cyclonedds/src/internal/ffi.rs
@@ -0,0 +1,52 @@
+//! Safe and ergonomic wrappers for the functionality from [`cyclonedds_sys`].
+//!
+//! This module is exposed to make it easier for applications to poke at the
+//! low-level implementation details of Cyclone DDS.
+
+#![allow(unsafe_code)]
+
+use crate::Result;
+use crate::error::IntoError;
+
+///
+pub fn dds_get_instance_handle(
+    entity: cyclonedds_sys::dds_entity_t,
+) -> Result<cyclonedds_sys::dds_instance_handle_t> {
+    let mut handle = 0;
+    unsafe { cyclonedds_sys::dds_get_instance_handle(entity, &mut handle) }.into_error()?;
+    Ok(handle)
+}
+
+///
+pub fn dds_get_status_changes(entity: cyclonedds_sys::dds_entity_t) -> Result<u32> {
+    let mut status = 0;
+    unsafe { cyclonedds_sys::dds_get_status_changes(entity, &mut status) }.into_error()?;
+    Ok(status)
+}
+
+///
+pub fn dds_read_status(entity: cyclonedds_sys::dds_entity_t, mask: u32) -> Result<u32> {
+    let mut status: u32 = 0;
+    unsafe { cyclonedds_sys::dds_read_status(entity, &mut status, mask) }.into_error()?;
+    Ok(status)
+}
+
+///
+pub fn dds_take_status(entity: cyclonedds_sys::dds_entity_t, mask: u32) -> Result<u32> {
+    let mut status: u32 = 0;
+    unsafe { cyclonedds_sys::dds_take_status(entity, &mut status, mask) }.into_error()?;
+    Ok(status)
+}
+
+///
+pub fn dds_get_status_mask(entity: cyclonedds_sys::dds_entity_t) -> Result<u32> {
+    let mut mask: u32 = 0;
+    unsafe { cyclonedds_sys::dds_get_status_mask(entity, &mut mask) }.into_error()?;
+    Ok(mask)
+}
+
+///
+pub fn dds_set_status_mask(entity: cyclonedds_sys::dds_entity_t, mask: u32) -> Result<()> {
+    unsafe { cyclonedds_sys::dds_set_status_mask(entity, mask) }.into_error()?;
+    Ok(())
+}
