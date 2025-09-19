8d0fd961a0b45b9d9635f168fcfd701ae26d4849
Troy Karan Harrison <troy.harrison@zettascale.tech>
2025-09-19 12:51:17 +0200

    Add listeners
    Commit-type: feature
    Scope: cyclonedds

--- cyclonedds/src/writer.rs
+++ cyclonedds/src/writer.rs
@@ -68,6 +68,41 @@ impl<'d, 'p, 't, T> Writer<'d, 'p, 't, T> {
             phantom_topic: std::marker::PhantomData,
         })
     }
+
+    pub fn set_listener<L>(&mut self, listener: L) -> Result<()>
+    where
+        T: serde::ser::Serialize
+            + serde::de::DeserializeOwned
+            + std::clone::Clone
+            + std::default::Default
+            + std::fmt::Debug,
+        L: AsRef<crate::WriterListener<T>>,
+    {
+        listener
+            .as_ref()
+            .as_ffi()
+            .map(|listener| ffi::dds_set_listener(self.inner, Some(listener.inner)))
+            .flatten()
+    }
+
+    ///
+    pub fn unset_listener(&mut self) -> Result<()> {
+        ffi::dds_set_listener(self.inner, None)?;
+        Ok(())
+    }
+
+    ///
+    pub fn with_listener<L>(mut self, listener: L) -> Result<Self>
+    where
+        T: serde::ser::Serialize
+            + serde::de::DeserializeOwned
+            + std::clone::Clone
+            + std::default::Default
+            + std::fmt::Debug,
+        L: AsRef<crate::WriterListener<T>>,
+    {
+        self.set_listener(listener).map(|_| self)
+    }
 }
 
 impl<T> Drop for Writer<'_, '_, '_, T> {
@@ -175,5 +210,62 @@ mod tests {
         writer.inner = writer_id;
 
         assert_eq!(result, crate::Error::BadParameter);
+    }
+
+    #[test]
+    fn test_writer_with_listener() {
+        let domain_id = crate::tests::domain::unique_id();
+        let domain = crate::Domain::new(domain_id).unwrap();
+        let topic_name = crate::tests::topic::unique_name();
+        let participant = crate::Participant::new(&domain).unwrap();
+        let topic = Topic::<crate::tests::topic::Data>::new(&participant, &topic_name).unwrap();
+
+        let listener = crate::WriterListener::new()
+            .with_liveliness_lost(|_, _| ())
+            .with_offered_deadline_missed(|_, _| ())
+            .with_offered_incompatible_qos(|_, _| ())
+            .with_publication_matched(|_, _| ());
+
+        let _ = Writer::new(&participant, &topic)
+            .unwrap()
+            .with_listener(&listener)
+            .unwrap();
+
+        let mut writer = Writer::new(&participant, &topic).unwrap();
+        writer.set_listener(&listener).unwrap();
+        writer.unset_listener().unwrap();
+    }
+
+    #[test]
+    fn test_writer_with_listener_on_invalid_writer() {
+        let domain_id = crate::tests::domain::unique_id();
+        let domain = crate::Domain::new(domain_id).unwrap();
+        let topic_name = crate::tests::topic::unique_name();
+        let participant = crate::Participant::new(&domain).unwrap();
+        let topic = Topic::<crate::tests::topic::Data>::new(&participant, &topic_name).unwrap();
+
+        let listener = crate::WriterListener::new();
+
+        let mut writer = Writer::new(&participant, &topic).unwrap();
+        let writer_id = writer.inner;
+        writer.inner = 0;
+        let result = writer.set_listener(&listener).unwrap_err();
+        assert_eq!(result, crate::Error::BadParameter);
+        let result = writer.unset_listener().unwrap_err();
+        assert_eq!(result, crate::Error::BadParameter);
+        writer.inner = writer_id;
+    }
+
+    #[test]
+    fn test_writer_create_from_existing() {
+        let domain_id = crate::tests::domain::unique_id();
+        let domain = crate::Domain::new(domain_id).unwrap();
+        let topic_name = crate::tests::topic::unique_name();
+        let participant = crate::Participant::new(&domain).unwrap();
+        let topic = Topic::<crate::tests::topic::Data>::new(&participant, &topic_name).unwrap();
+
+        let writer_01 = Writer::new(&participant, &topic).unwrap();
+        let writer_02 = Writer::<crate::tests::topic::Data>::from_existing(writer_01.inner);
+        assert_eq!(writer_01.inner, writer_02.inner);
     }
 }
